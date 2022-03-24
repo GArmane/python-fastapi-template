030569d8e429ceb5ea1a276ca92ea29c11947bc1
Giovanni Armane <giovanniarmane@gmail.com>
2022-03-24 14:48:28 -0300

    test(tests/api/conftest.py): async test client fixture

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -3,6 +3,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.api.app import init_app
+from httpx import AsyncClient
 
 
 @pytest.fixture()
@@ -15,3 +16,10 @@ def web_app(env_settings):
 def test_client(web_app):
     """Return web application test client."""
     return TestClient(web_app)
+
+
+@pytest.fixture()
+async def async_test_client(web_app):
+    """Return an async test client."""
+    async with AsyncClient(app=web_app, base_url="http://") as ac:
+        yield ac
